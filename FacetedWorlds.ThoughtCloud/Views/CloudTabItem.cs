5920f2c3a20773cad3eb8814127b6a0fadecfade
michaellperry <mperry@adventuresinsoftware.com>
2011-05-25 18:01:27 -0500

    Added controls to the cloud view.

--- FacetedWorlds.ThoughtCloud/Views/CloudTabItem.cs
+++ FacetedWorlds.ThoughtCloud/Views/CloudTabItem.cs
@@ -8,15 +8,13 @@ namespace FacetedWorlds.ThoughtCloud.Views
     public class CloudTabItem : TabItem
     {
         private CloudTabViewModel _viewModel;
-        private CloudView _content;
         private Dependent _depHeader;
 
         public CloudTabItem(CloudTabViewModel viewModel)
         {
             _viewModel = viewModel;
 
-            _content = new CloudView();
-            _content.DataContext = _viewModel.Content;
+            Content = new CloudView { DataContext = _viewModel.Content };
 
             _depHeader = new Dependent(() => Header = _viewModel.Header);
             _depHeader.Invalidated += () => Deployment.Current.Dispatcher.BeginInvoke(() => _depHeader.OnGet());
