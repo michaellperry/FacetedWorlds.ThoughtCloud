de24c1d18fd17ae5e37fe9689011694353908c6b
Michael L Perry <mperry@adventuresinsoftware.com>
2011-06-02 23:25:38 -0500

    Animated architecture view.

---
+++ ThoughtCloud.Presentation/Views/ArchitectureView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ThoughtCloud_Presentation.Views
+{
+    public partial class ArchitectureView : UserControl
+    {
+        public ArchitectureView()
+        {
+            InitializeComponent();
+        }
+    }
+}
