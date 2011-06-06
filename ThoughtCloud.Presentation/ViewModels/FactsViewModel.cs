fc0592b1818d8e51b79901c23e9e290438bd848e
Michael L Perry <mperry@adventuresinsoftware.com>
2011-06-06 18:38:36 -0500

    Show code while animating fact tree.

--- ThoughtCloud.Presentation/ViewModels/FactsViewModel.cs
+++ ThoughtCloud.Presentation/ViewModels/FactsViewModel.cs
@@ -1,15 +1,7 @@
-﻿using System.Windows.Input;
-using UpdateControls.Fields;
-using UpdateControls.XAML;
-using System;
+﻿using UpdateControls.Fields;
 
 namespace ThoughtCloud_Presentation.ViewModels
 {
-    public interface IPresentationViewModel
-    {
-        bool Backward();
-        bool Forward();
-    }
     public class FactsViewModel : IPresentationViewModel
     {
         public enum StateId
@@ -60,6 +52,29 @@ namespace ThoughtCloud_Presentation.ViewModels
         {
             get
             {
+                switch (_state.Value)
+                {
+                    case StateId.NewIdentityMike:
+                        return "Identity mike = Community.AddFact(\nnew Identity(\"mike\"));";
+                    case StateId.NewIdentityRussell:
+                        return "Identity russell = Community.AddFact(\nnew Identity(\"russell\"));";
+                    case StateId.NewCloud1:
+                        return "Cloud cloud1 = Community.AddFact(new Cloud());";
+                    case StateId.NewCloud2:
+                        return "Cloud cloud2 = Community.AddFact(new Cloud());";
+                    case StateId.NewThought3:
+                        return "Thought thought3 = Community.AddFact(\nnew Thought(cloud1));";
+                    case StateId.NewThought3Text:
+                        return "thought3.Text = \"Star Wars\";";
+                    case StateId.NewThought4:
+                        return "Thought thought4 = Community.AddFact(\nnew Thought(cloud1));";
+                    case StateId.NewThought4Text:
+                        return "thought4.Text = \"Characters\";";
+                    case StateId.NewLink:
+                        return "Community.AddFact(new Link(\nnew List<Thought> { thought3, thought4 }));";
+                    case StateId.NewShare:
+                        return "Community.AddFact(new Share(russell, cloud1));";
+                }
                 return string.Empty;
             }
         }
